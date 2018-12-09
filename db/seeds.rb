90fb58a8d137b872f2bab3f9e6e415ac48ce418b
Rei Ramos <reiallenramos@gmail.com>
2018-12-09 14:37:23 +0800

    Create Stocks seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+def create_rap_user
+  u = User.create(
+    email: "reiallenramos@gmail.com",
+    password: "123123"
+  )
+  puts "RAP user created" if u.save
+end
+
+def seed_stocks
+  csv_text = File.read(Rails.root.join('lib', 'seeds', 'stocks.csv'))
+  csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+
+  csv.each do |row|
+    s = Stock.new
+    s.symbol = row['symbol']
+    s.name = row['name']
+
+    puts "<#{s.symbol}> #{s.name} saved" if s.save
+  end
+end
+
+case Rails.env
+when "development"
+  create_rap_user
+  seed_stocks
+when "production"
+end
