37ec2d5257546d041e661a39cbe0c78de571f05b
Rei Ramos <reiallenramos@gmail.com>
2018-12-09 23:08:08 +0800

    Show Stock via modal

--- app/javascript/components/Stocks.js
+++ app/javascript/components/Stocks.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 import axios from 'axios'
-import { Table, PageHeader, Button }from 'react-bootstrap'
+import { Table, PageHeader, Button, ButtonGroup, Modal }from 'react-bootstrap'
 
 var base_stocks_url = 'http://192.168.0.14:3000/api/v1';
 
@@ -8,11 +8,16 @@ const csrfToken = document.querySelector('[name="csrf-token"]').content;
 axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
 
 class Stocks extends React.Component {
-  constructor() {
-    super();
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShowModal = this.handleShowModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
 
     this.state = {
-      stocks: []
+      stocks: [],
+      showModal: false,
+      current_stock: null
     }
   }
 
@@ -24,11 +29,21 @@ class Stocks extends React.Component {
       })
   }
 
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
+
+  handleShowModal(stock) {
+    this.setState({ current_stock: stock });
+    this.setState({ showModal: true });
+  }
+
   handleDeleteStock(id) {
     axios.delete(`${base_stocks_url}/stocks/${id}.json`)
       .then(res => {
         console.log('successfully deleted!');
         this.removeStockClient(id);
+        this.handleCloseModal();
       })
   }
 
@@ -42,9 +57,35 @@ class Stocks extends React.Component {
 
   render() {
     const { stocks } = this.state;
+    let modal;
+
+    if (this.state.current_stock == null ) {
+      modal = null
+    } else {
+      modal = (
+        <Modal show={this.state.showModal} onHide={this.handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.state.current_stock.symbol}</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>{this.state.current_stock.name}</Modal.Body>
+
+          <Modal.Footer>
+            <Button onClick={this.handleCloseModal}>Close</Button>
+            <Button
+              bsStyle="danger"
+              onClick={() => this.handleDeleteStock(this.state.current_stock.id)}
+              >
+              <span className="glyphicon glyphicon-trash"></span>
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )
+    }
 
     return (
       <Fragment>
+        {modal}
         <PageHeader>
           Stock List
         </PageHeader>
@@ -63,12 +104,20 @@ class Stocks extends React.Component {
                 <td>{stock.name}</td>
                 <td>
                   <div className="text-center">
-                    <Button
-                      bsStyle="danger"
-                      onClick={() => this.handleDeleteStock(stock.id)}
-                    >
-                    <span className="glyphicon glyphicon-trash"></span>
-                    </Button>
+                    <ButtonGroup>
+                      <Button
+                        bsStyle="info"
+                        onClick={() => this.handleShowModal(stock)}
+                      >
+                      <span className="glyphicon glyphicon-eye-open"></span>
+                      </Button>
+                      <Button
+                        bsStyle="danger"
+                        onClick={() => this.handleDeleteStock(stock.id)}
+                      >
+                      <span className="glyphicon glyphicon-trash"></span>
+                      </Button>
+                    </ButtonGroup>
                   </div>
                 </td>
               </tr>
