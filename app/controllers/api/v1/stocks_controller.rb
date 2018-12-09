8fd8909ad9e4e988b580621f8e6e8b884bf2bda2
Rei Ramos <reiallenramos@gmail.com>
2018-12-09 20:44:30 +0800

    Apply namespace to Rails routes to play well with React Router

--- app/controllers/api/v1/stocks_controller.rb
+++ app/controllers/api/v1/stocks_controller.rb
@@ -1,4 +1,4 @@
-class StocksController < ApplicationController
+class Api::V1::StocksController < ApplicationController
   def index
     stocks = Stock.all.order('symbol ASC')
     render json: stocks.to_json
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get '/users/check_for_user' => "users#check_for_user"
 
-  resources :stocks
+  namespace :api do
+    namespace :v1 do
+      resources :stocks
+    end
+  end
 
   devise_for :users
   root 'welcome#index'
+  match '*path', to: 'welcome#index', via: :all
 end
