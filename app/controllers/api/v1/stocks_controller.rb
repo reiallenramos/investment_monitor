7586d309fb13a39f39e55762293719807621bb5d
Rei Ramos <reiallenramos@gmail.com>
2018-12-10 20:50:23 +0800

    Skip CSRF verification

--- app/controllers/api/v1/stocks_controller.rb
+++ app/controllers/api/v1/stocks_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::StocksController < Api::V1::BaseController
   before_action :set_stock, only: %i[destroy]
+  skip_before_action :verify_authenticity_token
+
   def index
     respond_with Stock.all.order('symbol ASC')
   end
