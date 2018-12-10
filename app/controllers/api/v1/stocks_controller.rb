32b7662b731a03909e0c68f8c1ca491dfa9596d9
Rei Ramos <reiallenramos@gmail.com>
2018-12-10 20:51:02 +0800

    Implement http POST

--- app/controllers/api/v1/stocks_controller.rb
+++ app/controllers/api/v1/stocks_controller.rb
@@ -6,11 +6,31 @@ class Api::V1::StocksController < Api::V1::BaseController
     respond_with Stock.all.order('symbol ASC')
   end
 
+  def new
+    respond_with Stock.new
+  end
+
+  def create
+    @stock = Stock.new(stock_params)
+
+    if @stock.save
+      render json: @stock.to_json
+    else
+      render json: { status: "error", message: @stock.errors }
+    end
+  end
+
   def destroy
     respond_with @stock.destroy
   end
 
   private
+    def stock_params
+      params.require(:stock).permit(
+        :name,
+        :symbol
+      )
+    end
 
     def set_stock
       @stock = Stock.find(params[:id])
