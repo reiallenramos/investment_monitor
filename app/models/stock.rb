dca08d4f86954a31c503cbab77bbb71e31b53f28
Rei Ramos <reiallenramos@gmail.com>
2018-12-15 14:20:37 +0800

    Implement stock_history_of User

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,4 +3,12 @@ class Stock < ApplicationRecord
 
   validates_presence_of :name, :symbol, allow_blank: false
   validates_uniqueness_of :name, :symbol
+
+  # returns array of unique Stocks that user has interacted with (buy and/or sell)
+  def self.stock_history_of(user)
+    stock_ids = user.buy_entries.map(&:stock_id).uniq
+    # stock_ids << user.sell_entries.map(&:stock_id).uniq
+
+    Stock.where(id: [stock_ids])
+  end
 end
