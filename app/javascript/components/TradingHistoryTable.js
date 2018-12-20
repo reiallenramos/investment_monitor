b2b1c9e0c50dc17187c752e20b57fd7e6f709612
Rei Ramos <reiallenramos@gmail.com>
2018-12-20 23:03:42 +0800

    Include SellEntries in TradingHistoryTable, implement conditional styling to cells

--- app/javascript/components/TradingHistoryTable.js
+++ app/javascript/components/TradingHistoryTable.js
@@ -3,12 +3,16 @@ import axios from 'axios'
 import * as constants from './constants'
 import ReactTable from "react-table";
 
+const GREEN = '3px solid rgba(82,210,154,1)';
+const ORANGE = '3px solid rgba(235,118,85,1)';
+
 class TradingHistoryTable extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       buy_entries: null,
+      sell_entries: null,
       stockId: this.props.stockId,
       currentUserId: this.props.currentUserId
     }
@@ -24,29 +28,50 @@ class TradingHistoryTable extends React.Component {
         })
 
         this.setState({ buy_entries });
-      })
+      }
+    )
+
+    axios.get(`${constants.REQUEST_URL}/sell_entries/by_user_and_stock.json?user_id=${this.state.currentUserId}&stock_id=${this.state.stockId}`)
+      .then( res => {
+        var sell_entries = res.data;
+
+        sell_entries.forEach((entry) => {
+          entry.type = 'SELL'
+        })
+
+        this.setState({ sell_entries });
+      }
+    )
+  }
+
+  getProps = (state, rowInfo) => {
+    if (rowInfo && rowInfo.row) {
+      return {
+        style: {
+          borderLeft:
+            rowInfo.row.type === "BUY" ? GREEN : ORANGE
+        }
+      };
+    } else {
+      return {};
+    }
   }
 
   render() {
-    const { buy_entries } = this.state;
+    var buy_and_sell_entries = [];
+    if (this.state.buy_entries != null && this.state.sell_entries != null) {
+      buy_and_sell_entries = (this.state.buy_entries).concat(this.state.sell_entries);
+    }
+
     let table;
 
     const columns = [{
+      getProps: this.getProps,
       Header: 'Date',
       accessor: 'trade_date'
     },{
       Header: 'Transaction',
-      accessor: 'type',
-      Cell: row => (
-        <div style={{
-          color: 
-          row.value == 'BUY'
-          ? 'green'
-          : 'sell'
-          }}>
-          {row.value}
-        </div>
-      )
+      accessor: 'type'
     },{
       Header: 'Quantity',
       accessor: 'quantity',
@@ -61,12 +86,12 @@ class TradingHistoryTable extends React.Component {
       accessor: 'net_amount'
     }]
 
-    if (this.state.buy_entries == null) {
+    if (this.state.buy_entries == null && this.state.sell_entries == null) {
       table = (
         <div>Loading...</div>
       )
     } else {
-      table = <ReactTable data={buy_entries} columns={columns} defaultPageSize={5}/>
+      table = <ReactTable data={buy_and_sell_entries} columns={columns} defaultPageSize={5} className="-striped -highlight" />
     }
 
     return (
